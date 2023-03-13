1c471ef4739e98549d0edd06ab03f4eb83d9a62c
Mikael Turøy <mikael.turoy@holbergeeg.org>
2023-03-13 22:19:27 +0100

    LLaMA 13b works with 4bit

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Workaround for issue: https://github.com/qwopqwop200/GPTQ-for-LLaMa/issues/31
-sed -i 's/if checkpoint\.endswith/if checkpoint\.as_posix\(\).endswith/' /app/repositories/GPTQ-for-LLaMa/llama.py
+# sed -i 's/if checkpoint\.endswith/if checkpoint\.as_posix\(\).endswith/' /app/repositories/GPTQ-for-LLaMa/llama.py
 
 cd /app/repositories/GPTQ-for-LLaMa && python setup_cuda.py install
 cd /app
 
-python server.py --load-in-4bit --listen --listen-port=8888 --model=llama-13b
+python server.py --cai-chat --load-in-4bit --listen --listen-port=8888 --model=llama-13b
