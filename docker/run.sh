fdce6b19273393c4f9eb626d09e5ae20fa25ee3b
Mikael Turøy <mikael.turoy@holbergeeg.org>
2023-03-27 14:14:12 +0200

    New 4bit

--- docker/run.sh
+++ docker/run.sh
@@ -3,23 +3,19 @@
 # Workaround for issue: https://github.com/qwopqwop200/GPTQ-for-LLaMa/issues/31
 # sed -i 's/if checkpoint\.endswith/if checkpoint\.as_posix\(\).endswith/' /app/repositories/GPTQ-for-LLaMa/llama.py
 
-model="llama-7b"
+model="alpaca-native-4bit"
 
 if [ ! -e "/app/models/$model" ] ; then
     echo "Downloading LLaMa model $model (metadata)"
-    if [ ! -e "/app/models/$model-hf" ] ; then
-        python download-model.py --text-only decapoda-research/$model-hf
+    if [ ! -e "/app/models/$model" ] ; then
+        python download-model.py --text-only ozcur/$model
     fi
-    mv /app/models/$model-hf /app/models/$model
 fi
-if [ ! -e "/app/models/$model-4bit.pt" ] ; then
-    echo "Downloading LLaMa model $model (weights)"
-    wget https://huggingface.co/decapoda-research/$model-hf-int4/resolve/main/$model-4bit.pt -O /app/models/$model-4bit.pt --progress=dot:giga
-fi
+# if [ ! -e "/app/models/$model-4bit.pt" ] ; then
+#     echo "Downloading LLaMa model $model (weights)"
+#     wget https://huggingface.co/decapoda-research/$model-hf-int4/resolve/main/$model-4bit.pt -O /app/models/$model-4bit.pt --progress=dot:giga
+# fi
 
-echo "Fixing LLaMa models (tokenizer)"
-cd /app/models
-find . -name '*.json' -exec sed -i -e 's/LLaMATokenizer/LlamaTokenizer/g' {} \;
 
 if [ ! -e "/opt/conda/lib/libcudart.so" ] ; then
     echo "Fixing symlink for libcudart.so"
@@ -30,4 +26,4 @@ fi
 cd /app/repositories/GPTQ-for-LLaMa && python setup_cuda.py install
 cd /app
 
-python server.py --cai-chat --gptq-bits 4 --listen --listen-port=8889 --model=$model
+python server.py --cai-chat --wbits 4 --listen --listen-port=8889 --model=$model --groupsize=128
